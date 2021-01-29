4b6e68dc6bfa7ab9bf4708731288e75aa53f2d35
James Zhang <liulfbaby@gmail.com>
2021-01-30 00:14:57 +0800

    Change port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 app.listen(port);
 
 console.log(`Listening to http://localhost:${port}`);
