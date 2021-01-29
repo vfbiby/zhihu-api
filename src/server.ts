bd9ae18b6b6d9cdfc5f4945ae6fbb734d043f473
James Zhang <liulfbaby@gmail.com>
2021-01-29 12:02:30 +0800

    handle router manuly

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import Koa from "koa";
 const app = new Koa();
 
-app.use(async (ctx: any) => {
-  ctx.body = "Hello James Zhang!";
+app.use(async (ctx: any, next: Koa.Next) => {
+  if (ctx.url === "/") {
+    ctx.body = "this is homepage";
+  } else if (ctx.url === "/users") {
+    ctx.body = "this is user page";
+  } else if (ctx.url.match(/\/users\/\w+/)) {
+    const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+    ctx.body = `this is user ${userId}`;
+  } else {
+    ctx.status = 404;
+  }
 });
 
 app.listen(3333);
