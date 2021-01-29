c5b78eb0d28d364542c992dad0dc40b28284fc59
James Zhang <liulfbaby@gmail.com>
2021-01-29 22:02:28 +0800

    Add server test

---
+++ tests/server.test.ts
@@ -0,0 +1,14 @@
+import app from "../src/app";
+
+const mockListen = jest.fn();
+app.listen = mockListen;
+
+afterEach(() => {
+  mockListen.mockReset();
+});
+
+test("Server works", async () => {
+  require("../src/server");
+  expect(mockListen.mock.calls.length).toBe(1);
+  expect(mockListen.mock.calls[0][0]).toBe(process.env.PORT || 3000);
+});
