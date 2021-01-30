7e6f9a880ee885673008932cca4897f90042c45c
James Zhang <liulfbaby@gmail.com>
2021-01-30 11:42:26 +0800

    Fix: third-party module declaration file not found in ts-node

---
+++ src/@types/koa-parameter/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+
+declare module "koa-parameter" {
+  export default function parameter(app: any, translate?: any): any;
+}
--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,18 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import registerRouters from "./Routers/IndexRouter";
 import error from "koa-json-error";
+import parameter from "koa-parameter";
 
 const app = new Koa();
 
 app.use(
   error({
-    postFormat: (e, { stack, ...rest }) =>
+    postFormat: (_, { stack, ...rest }) =>
       process.env.NODE_ENV === "production" ? rest : { stack, ...rest },
   })
 );
 app.use(bodyParser());
+app.use(parameter(app));
 registerRouters(app);
 
 export = app;
