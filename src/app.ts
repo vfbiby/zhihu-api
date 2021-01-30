f7155468dc2747949dcf6a6160a32ac4e7291766
James Zhang <liulfbaby@gmail.com>
2021-01-30 18:04:54 +0800

    Use mongoose connect to mongodb

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,19 @@ import bodyParser from "koa-bodyparser";
 import registerRouters from "./Routers/IndexRouter";
 import error from "koa-json-error";
 import parameter from "koa-parameter";
+import mongoose from "mongoose";
+import config from "./config";
 
 const app = new Koa();
+const { connectionUrl } = config;
+mongoose.connect(
+  connectionUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Mongodb connect success!");
+  }
+);
+mongoose.connection.on("error", console.error);
 
 app.use(
   error({
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const config = {
+  connectionUrl:
+    "mongodb+srv://vfbiby:123456ab@zhihu.mmnhw.mongodb.net/zhihu?retryWrites=true&w=majority",
+};
+
+export default config;
