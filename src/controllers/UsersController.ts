4add4471f6e9c183c9ac238857d1bf1ca5bcc78d
James Zhang <liulfbaby@gmail.com>
2021-02-05 17:18:03 +0800

    feat: fuzzy search on users and topics

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -8,7 +8,7 @@ class UsersController {
     const { per_page = 10 } = ctx.query;
     const page = Math.max(ctx.query.page * 1, 1) - 1;
     const perPage = Math.max(per_page * 1, 1);
-    ctx.body = await User.find()
+    ctx.body = await User.find({ name: new RegExp(ctx.query.q) })
       .limit(perPage)
       .skip(page * perPage);
   }
